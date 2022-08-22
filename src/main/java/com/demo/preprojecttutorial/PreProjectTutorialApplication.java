2b92c08a2a5bb56d23696fbd6c3135139f52e43b
penrose15 <seongju99@naver.com>
2022-08-22 14:08:20 +0900

    To-Do Application!

---
+++ src/main/java/com/demo/preprojecttutorial/PreProjectTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.demo.preprojecttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProjectTutorialApplication.class, args);
+	}
+
+}
